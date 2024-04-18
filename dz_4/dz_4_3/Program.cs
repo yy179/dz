f303f11f6ca6f0cf3448460ef1a2c75238d36fd6
yy739 <sslol10007@gmail.com>
2024-04-18 20:00:02 +0300

    4

--- dz_4/dz_4_3/Program.cs
+++ dz_4/dz_4_3/Program.cs
@@ -9,7 +9,7 @@
             int num = int.Parse(Console.ReadLine());
             if (num >= 1 && num <= 100) 
             { 
-                string outp = (num % 15 == 0) ? "FizzBuzz" : (num % 5 == 0) ? "Buzz" : (num % 3 == 0) ? "Fizz" : "Число не кратне 15,5,3";
+                string outp = (num % 15 == 0) ? "FizzBuzz" : (num % 3 == 0) ? "Buzz" : (num % 5 == 0) ? "Fizz" : "Число не кратне 15,5,3";
                 Console.WriteLine(outp);
             }
             else { Console.Write("Число вiд 1 до 100"); }
