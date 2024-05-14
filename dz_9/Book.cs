9db2b66ed42a84fa9c6e86fd18ae38a6d0f6df0b
yy739 <sslol10007@gmail.com>
2024-05-14 18:04:52 +0300

    f

---
+++ dz_9/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_9
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+        private int year;
+        private int pages;
+        public Book(string title, string author, int year, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.year = year;
+            this.pages = pages;
+        }
+        public void DisplayInfo() { Console.WriteLine($"Title: {title}\nAuthor: {author}\nYear: {year}\nPages: {pages}\nIsThick: {isThick()}\n"); }
+        public string isThick() 
+        {
+            int pages = this.pages;
+            if (pages <= 500) {  return "Less than 500 pages"; }
+            else { return "More than 500 pages"; }
+        }
+        
+
+    }
+}
---
+++ dz_9/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace dz_9
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Book book1 = new Book("Title_","Author_",1990,600);
+           book1.DisplayInfo();
+           Book book2 = new Book("Title__", "Author__", 1990, 300);
+           book2.DisplayInfo();
+           Console.ReadKey();
+        }
+    }
+}
