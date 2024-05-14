fe924c8875c64f2f5c4d168b66e1a1de55969978
yy739 <sslol10007@gmail.com>
2024-05-14 19:59:12 +0300

    zc

---
+++ dz_10/dz_10_1/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_10_1
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+        private string sex;
+        public Person(string name, int age, string sex)
+        {
+            this.name = name;
+            this.age = age;
+            this.sex = sex;
+        }
+        public Person(string name) : this(name, 0, "Not indicated") {}
+        public int Ages
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Person(string name, int age) : this (name, age, "Not indicated"){}
+        public void PrintDetails() { Console.WriteLine($"Name: {name}\nAge: {age}\nSex: {sex}\nIsAdult: {isAdult()}\n"); }
+        public bool isAdult()
+        {
+            return age >= 18;
+        }
+        private string ChangeName(string name) 
+        {
+            Console.WriteLine("New name: ");
+            this.name = Console.ReadLine();
+            return this.name;
+        }
+    }
+}
---
+++ dz_10/dz_10_1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace dz_10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "y";
+            while (answer == "y")
+            {
+                Console.WriteLine("Input Name: ");
+                string n = Convert.ToString(Console.ReadLine());
+                Console.Write("Input age: ");
+                int d = int.Parse(Console.ReadLine());
+                Person person1 = new Person(n, d);
+                person1.Ages = d;
+                person1.Name = n;
+                person1.PrintDetails();
+                Console.WriteLine("Wanna change name? Y/N");
+                answer = Console.ReadLine().ToLower();
+            }
+
+            Person person2 = new Person("Vlad", 18);
+            person2.PrintDetails();
+
+            Person person3 = new Person("Vlad", 18, "Male");
+            person3.PrintDetails();
+        }
+    }
+}
